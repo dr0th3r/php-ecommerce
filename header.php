54af1be4ea460896ad829f6915752d46567975e5
dr0th3r <dr0th3r.code@gmail.com>
2024-03-09 15:13:28 +0100

    Add header

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./static/header.css">
+</head>
+<body>
+  <nav class="header-nav">
+    <div class="header-top">
+      <h2>Super Shop</h2>
+      <div class="search">
+        <input type="text" placeholder="Search" class="search-bar">
+        <button class="icon-btn"><i class="fas fa-search"></i></button>
+      </div>
+      <div class="user">
+        <button class="icon-btn"><i class="fas fa-shopping-cart"></i></button>
+        <button class="icon-btn"><i class="fas fa-user"></i></button>
+      </div>
+    </div>
+    <ul class="header-links">
+      <li class="product">Mobile</li>
+      <li class="product">PC, Notebook</li>
+      <li class="product">Hardware</li>
+      <li class="product">TV, audio, photo</li>
+      <li class="product">Gadgets</li>
+      <li class="product">Gaming</li>
+    </ul>
+  </nav>
+</body>
+</html>
