8dcf89833805113a07d4cd63edd262a691c91266
dr0th3r <dr0th3r.code@gmail.com>
2024-03-10 16:46:50 +0100

    Add product reviews

--- product/index.php
+++ product/index.php
@@ -1,10 +1,13 @@
 <?php
+  session_start();
   include_once("../config/db.php");
 
   if(!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname)) {
     echo "Failed to connect!";
     die();
   }
+
+  $user_id = $_SESSION['user_id'];
 
   $id = $_GET['id'];
 
@@ -13,10 +16,37 @@
     die;
   }
 
-  $query = "select product.*, brand.name as brand_name from product inner join brand on product.brand_id = brand.id where product.id = $id;";
-  $result = mysqli_query($con, $query);
-  if ($result) {
-    $product = mysqli_fetch_assoc($result);
+/*   $productQuery = "select 
+              product.*, 
+              brand.name as brand_name,
+              review.id as review_id, review.comment as review_comment, review.rating as review_rating, review.user_id as user_id,
+              user.email as user_email
+            from product 
+            left join brand on product.brand_id = brand.id 
+            left join review on product.id = review.product_id
+            left join user on review.user_id = user.id
+            where product.id = $id
+            order by user_id,
+              case when user_id = $user_id then 0 else 1 end;
+            "; */
+
+  $productQuery = "select product.*, brand.name as brand_name from product join brand on product.brand_id = brand.id where product.id = $id;";
+  
+  $reviewsQuery = "select 
+    review.*, user.name as user_name 
+    from review 
+    join user on review.user_id = user.id 
+    where review.product_id = $id
+    order by user_id,
+      case when user_id = $user_id then 0 else 1 end;
+    ";
+
+  $productResult = mysqli_query($con, $productQuery);
+  $reviewsResult = mysqli_query($con, $reviewsQuery);
+
+  if ($productResult && $reviewsResult) {
+    $product = mysqli_fetch_assoc($productResult);
+    $reviews = mysqli_fetch_all($reviewsResult, MYSQLI_ASSOC);
   }
   else {
     echo "Failed to get product";
@@ -41,5 +71,17 @@
       <p>{$product['brand_name']}</p>
     HEREDOC;
   ?>
+
+  <h2>Reviews</h2>
+  <ul>
+    <?php
+      foreach ($reviews as $review) {
+        echo "<li>";
+        echo "<p>{$review['user_name']}</p>";
+        echo "<p>{$review['comment']}</p>";
+        echo "<p>{$review['rating']}</p>";
+        echo "</li>";
+      }
+    ?>
 </body>
 </html>
